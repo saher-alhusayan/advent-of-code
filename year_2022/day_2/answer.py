d1f94db6e20b29ef3f13a78a472e4cadd00750f8
saher-alhusayan <56968205+saher-alhusayan@users.noreply.github.com>
2022-12-02 18:25:10 +0000

    improve answer

--- year_2022/day_2/answer.py
+++ year_2022/day_2/answer.py
@@ -4,38 +4,21 @@ from input_data import rounds
 rounds_ = rounds.split('\n')
 list_of_rounds = [item.split(' ') for item in rounds_]
 
+RULES = {
+    ("A", "X"): 4,
+    ("A", "Y"): 8,
+    ("A", "Z"): 3,
+    ("B", "X"): 1,
+    ("B", "Y"): 5,
+    ("B", "Z"): 9,
+    ("C", "X"): 7,
+    ("C", "Y"): 2,
+    ("C", "Z"): 6,
+}
 # PART 1
 def determiine_if_player_two_wins(list_of_rounds: list[str]) -> bool:
+    return RULES.get((list_of_rounds[0], list_of_rounds[1]))
 
-    player_one = list_of_rounds[0]
-    player_two = list_of_rounds[1]
-
-    if player_one == "A" and player_two == "X":
-        return 3 + 1
-
-    if player_one == "A" and player_two == "Y":
-        return 6 + 2
-
-    if player_one == "A" and player_two == "Z":
-        return 0 + 3
-    
-    if player_one == "B" and player_two == "X":
-        return 0 + 1
-
-    if player_one == "B" and player_two == "Y":
-        return 3 + 2
-
-    if player_one == "B" and player_two == "Z":
-        return 6 + 3
-    
-    if player_one == "C" and player_two == "X":
-        return 6 + 1
-
-    if player_one == "C" and player_two == "Y":
-        return 0 + 2
-
-    if player_one == "C" and player_two == "Z":
-        return 3 + 3
 
 outcome = list(map(determiine_if_player_two_wins, list_of_rounds))
 
