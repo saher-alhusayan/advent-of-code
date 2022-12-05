b5a2b78a1b9055e19e1c871db427dd4160ae5d45
saher-alhusayan <56968205+saher-alhusayan@users.noreply.github.com>
2022-12-05 14:44:46 +0000

    add year 22 day 5 part 2

--- year_2022/day_5/answer.py
+++ year_2022/day_5/answer.py
@@ -1,6 +1,7 @@
 from input_data import moves
 from typing import List, Dict
 import re
+import copy
 
 crates = {
     1: ["S", "C", "V", "N"],
@@ -18,19 +19,32 @@ instructions = [re.findall(r"\d+", item) for item in instructs]
 
 
 def action_moves(
-    crates: Dict[int, List[str]], instructions: List[List[str]]
+    crates: Dict[int, List[str]], instructions: List[List[str]], part: int
 ) -> Dict[int, List[str]]:
     for instruction in instructions:
         number_of_crates_to_move, crate_from, crate_to = instruction
         stack_from = crates.get(int(crate_from))
         stack_to = crates.get(int(crate_to))
         stacks_to_move = stack_from[-int(number_of_crates_to_move) :]
-        stacks_to_move.reverse()
+        if len(stacks_to_move) > 1 and part == 2:
+            pass
+        else:
+            stacks_to_move.reverse()
         for stack in stacks_to_move:
             stack_to.append(stack)
         for stack in stacks_to_move:
             stack_from.pop(-1)
 
 
-action_moves(crates=crates, instructions=instructions)
-print([v[-1:] for v in crates.values()])
+# PART 1
+
+part_one_crates = copy.deepcopy(crates)
+action_moves(crates=part_one_crates, instructions=instructions, part=1)
+print([v[-1:] for v in part_one_crates.values()])
+
+
+# PART 2
+
+part_two_crates = copy.deepcopy(crates)
+action_moves(crates=part_two_crates, instructions=instructions, part=2)
+print([v[-1:] for v in part_two_crates.values()])
